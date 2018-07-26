0dcfb5a34d570a2fe8bed3564dde047835567d82
Yabir <yabirgb@gmail.com>
2018-07-27 00:07:14 +0200

    Buefy to manage bulma

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+//React Router
 import router from './routes.js'
+
+//Vuex moduke
 import store from './store/store.js'
+
+//Translations
 import i18n from './translations/i18n.js'
+
+//Css Bulma
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 
 router.beforeEach(function (to, from, next) {
     window.scrollTo(0, 0);
@@ -10,6 +20,8 @@ router.beforeEach(function (to, from, next) {
 });
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 new Vue({
   router,
